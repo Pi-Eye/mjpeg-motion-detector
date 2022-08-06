7df8b78de4f62ba76f31fca410390c72fa30a18f
bwu1324 <bwu1324@gmail.com>
2022-08-06 14:09:34 -0500

    feat(generate_gaussian): implement generate_gaussian

--- src/generate_gaussian.cc
+++ src/generate_gaussian.cc
@@ -1,5 +1,42 @@
 #include "generate_gaussian.hpp"
 
-Gaussian GenerateGaussian(unsigned int size) {}
+#include <cmath>
+#include <vector>
 
-Gaussian ScaleGaussian(Gaussian& gaussian, unsigned int scale) {}
+Gaussian GenerateGaussian(unsigned int size) {
+  // Calculate Kernal height and width
+  unsigned int kernel_size = (2 * size) + 1;
+
+  // Create empty 1D array for kernal
+  std::vector<double> kernel;
+
+  // Calculate Kernel Values
+  double sigma = 1;
+  double sum = 0;
+  int center = static_cast<int>(kernel_size) / 2;
+  for (int i = 0; i < kernel_size; i++) {
+    kernel.push_back(exp(-1 * ((i - center) * (i - center)) / (2 * sigma * sigma)));
+    sum += kernel.at(i);
+  }
+
+  // Normalize Values
+  for (int i = 0; i < kernel_size; i++) kernel.at(i) /= sum;
+
+  return {1, kernel};
+}
+
+Gaussian ScaleGaussian(Gaussian& gaussian, unsigned int scale) {
+  // Create resized 1D array for kernel
+  std::vector<double> kernel;
+
+  // Copy values over and scale
+  double sum = 0;
+  for (int i = 0; i < gaussian.kernel.size(); i++) {
+    for (int j = 0; j < scale; j++) {
+      unsigned int index = i * scale + j;
+      kernel.push_back(gaussian.kernel.at(i) / scale);
+    }
+  }
+
+  return {scale, kernel};
+}
